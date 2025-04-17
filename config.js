e20d448975cd13f6e6c1d07d176a7aa97f64b0d6
tranhainam2003 <accchinh06082003@gmail.com>
2025-04-17 21:19:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Tia nắng ấm chiếu qua kẽ lá 🥰',
+    desc: ' Còn Anh thì chỉ muốn hôn má em thôi 🤭💗 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
